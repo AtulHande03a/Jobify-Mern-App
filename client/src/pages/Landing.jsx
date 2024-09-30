31301d9ade62ecba4dfae79875cbc8d9606dcab7
Atul Kumar Hande <handeatul03a@gmail.com>
2024-09-30 18:55:08 +0530

    feat: added frontend and backend for app

---
+++ client/src/pages/Landing.jsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+import Wrapper from '../assets/wrappers/LandingPage'
+import main from '../assets/images/main.svg'
+import { Link } from 'react-router-dom'
+import { Logo } from '../components'
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Tired of maintaining track of your job applications. Fear not, we
+            got you.. Jobify, lets you keep track of your job applications and
+            allows you to track changes during your application process.
+          </p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn ">
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  )
+}
+
+export default Landing
